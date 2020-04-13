edeb5410c11221bafbab6c7d71b285530e9d982a
AkshayKumar15 <akshay.kumar@tekarch.com>
2020-04-13 13:52:57 -0700

    second commit

--- src/Day1/HelloWorld.java
+++ src/Day1/HelloWorld.java
@@ -5,7 +5,8 @@ public class HelloWorld {
 	// main (ctrl+space)
 	// syso (ctrl+space)
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println("Hello World!!");
 	}
 
